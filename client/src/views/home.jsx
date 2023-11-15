f139bf4fb242bfc9fee1c12089ff52bbefffe639
Ardianto <108719126+ardiantoosman4@users.noreply.github.com>
2023-11-16 05:45:45 +0700

    navbar section

--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import { getPopularMovies, getTopMovies } from "../helpers/tmdbAPI";
+import { useState } from "react";
+import { useEffect } from "react";
+import { TMDB_IMG } from "../CONSTANT";
+import Navbar from "../components/navbar";
 
 export default function Home() {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [topMovies, setTopMovies] = useState([]);
+  useEffect(() => {
+    setMovieDatas();
+  }, []);
+
+  async function setMovieDatas() {
+    let dataPopular = await getPopularMovies();
+    let dataTop = await getTopMovies();
+    setPopularMovies(dataPopular);
+    setTopMovies(dataTop);
+  }
+
+  console.log(topMovies);
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="text-white">Home</div>
+      </div>
+    </>
+  );
 }
