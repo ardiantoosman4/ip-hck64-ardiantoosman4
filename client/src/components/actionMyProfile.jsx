ec95810d8fa0ee185d95b468189102a03a9ec7ee
Ardianto <108719126+ardiantoosman4@users.noreply.github.com>
2023-11-16 11:07:14 +0700

    handle cancel order

--- client/src/components/actionMyProfile.jsx
+++ client/src/components/actionMyProfile.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import React from "react";
 import { URL_DATA } from "../CONSTANT";
+import swal from "sweetalert";
+import { useNavigate } from "react-router-dom";
 
 export default function ActionButton({ type, orderId }) {
+  const navigate = useNavigate();
   async function handlePay(id) {
     try {
       let { data } = await axios({
@@ -11,6 +14,19 @@ export default function ActionButton({ type, orderId }) {
         headers: { authorization: `Bearer ${localStorage.access_token}` },
       });
       window.snap.pay(data.snapToken);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function handleCancelPay(id) {
+    try {
+      let { data } = await axios({
+        method: "delete",
+        url: URL_DATA + `/my-profile/order/${id}`,
+        headers: { authorization: `Bearer ${localStorage.access_token}` },
+      });
+      navigate("/");
+      swal("Cancel Payment", "Success to cancel order movie", "success");
     } catch (error) {
       console.log(error);
     }
@@ -32,6 +48,7 @@ export default function ActionButton({ type, orderId }) {
             Pay Movie
           </button>
           <button
+            onClick={() => handleCancelPay(orderId)}
             className="btn btn-danger btn-sm ms-2"
             style={{ width: "100px" }}
           >
